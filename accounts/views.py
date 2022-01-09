bd6221a2ab7b5fbec9c3896afbad43f5dd6b9c40
Evan <ivan.emelyanov.94@gmail.com>
2022-01-08 20:24:11 -0800

    Created Register View

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,14 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.shortcuts import render, redirect
+
+def register_view(request):
+    form = UserCreationForm(request.POST or None)
+    if form.is_valid():
+        user_obj = form.save()
+        return redirect('/login')
+    context = {"form": form}
+    return render(request, "accounts/register.html", context)
 
 def login_view(request):
     if request.method == "POST":
@@ -18,6 +27,3 @@ def logout_view(request):
         logout(request)
         return redirect('/')
     return render(request, "accounts/logout.html", {})
-    
-def register_view(request):
-    return render(request, "accounts/register.html", {})
--- rargorand/urls.py
+++ rargorand/urls.py
@@ -18,7 +18,8 @@ from django.urls import path
 
 from accounts.views import (
     login_view,
-    logout_view
+    logout_view,
+    register_view
 )
 from drops.views import (
     drop_search_view,
@@ -35,4 +36,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', login_view),
     path('logout/', logout_view),
+    path('register/', register_view),
 ]
