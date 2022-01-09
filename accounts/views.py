2edc7f04883b76978a526c64d9d3f68eb8d97337
Evan <ivan.emelyanov.94@gmail.com>
2022-01-08 20:35:15 -0800

    Modified Login View

--- accounts/views.py
+++ accounts/views.py
@@ -12,15 +12,17 @@ def register_view(request):
 
 def login_view(request):
     if request.method == "POST":
-        username = request.POST.get("username")
-        password = request.POST.get("password")
-        user = authenticate(request, username=username, password=password)
-        if user is None:
-            context = {"error": "Invalid username or password"}
-            return render(request, "accounts/login.html", context)
-        login(request, user)
-        return redirect('/')
-    return render(request, "accounts/login.html", {})
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('/')
+    else:
+        form = AuthenticationForm(request)
+    context = {
+        "form": form
+    }
+    return render(request, "accounts/login.html", context)
 
 def logout_view(request):
     if request.method == "POST":
