31ac6874d575d3e01177ea4da80ee1dba76c549a
Evan <ivan.emelyanov.94@gmail.com>
2022-01-10 23:25:32 -0800

    Tried to fix home_view

--- rargorand/views.py
+++ rargorand/views.py
@@ -2,12 +2,12 @@ from django.http import HttpResponse
 from django.template.loader import render_to_string
 from drops.models import Drop
 
-drop_qs = Drop.objects.all()
-context = {
-    "obj_list": drop_qs,
-}
+#drop_qs = Drop.objects.all()
+#context = {
+#    "obj_list": drop_qs,
+#}
 
-HTML_STRING = render_to_string("home-view.html", context=context)
+HTML_STRING = "Hello"#render_to_string("home-view.html", context=context)
 
 def home_view(request, *args, **kwargs):
     return HttpResponse(HTML_STRING)
