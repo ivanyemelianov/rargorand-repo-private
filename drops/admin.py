c610a1a67c6154fd4a71dfb68b01d88cc4bfd02a
Evan <ivan.emelyanov.94@gmail.com>
2022-01-11 18:33:25 -0800

    Added the Timestamp field from Drop model

--- drops/admin.py
+++ drops/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from .models import Drop
 
 class DropAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title']
+    list_display = ['id', 'title', 'timestamp']
     search_fields = ['title', 'description']
 
 admin.site.register(Drop, DropAdmin)
--- drops/models.py
+++ drops/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
 class Drop(models.Model):
-    title = models.TextField()
+    title = models.CharField(max_length=120)
     description = models.TextField()
+    timestamp = models.DateField(auto_now=False, auto_now_add=True)
