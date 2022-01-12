b2a97eb9161811e9827f1232e46aaa1de9bd4643
Evan <ivan.emelyanov.94@gmail.com>
2022-01-11 20:42:41 -0800

    Added Slug field to the Drop Model

--- drops/models.py
+++ drops/models.py
@@ -1,6 +1,13 @@
 from django.db import models
+from django.utils.text import slugify
 
 class Drop(models.Model):
     title = models.CharField(max_length=120)
+    slug = models.SlugField(blank=True, null=True)
     description = models.TextField()
     timestamp = models.DateField(auto_now=False, auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if self.slug is None:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
