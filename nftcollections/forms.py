1423b8b382381c0be506bd67c0d74cf7c58c06b2
Evan <ivan.emelyanov.94@gmail.com>
2022-01-17 21:03:31 -0800

    Changing form fields style

--- nftcollections/forms.py
+++ nftcollections/forms.py
@@ -4,9 +4,28 @@ from django import forms
 from .models import NftCollection, Nft
 
 class NftCollectionForm(forms.ModelForm):
+    error_css_class = 'error-filed'
+    required_css_class = 'required-field'
+    name = forms.CharField(help_text='This is your help! <a href="/contact">Contact us</a>')
     class Meta:
         model = NftCollection
         fields = ['name', 'description']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # django-crispy-forms
+        for field in self.fields:
+            new_data = {
+                "placeholder": f'Collection {str(field)}',
+                "class": 'form-control'
+            }
+            self.fields[str(field)].widget.attrs.update(
+                new_data
+            )
+        # self.fields['name'].label = ''
+        # self.fields['name'].widget.attrs.update({'class': 'form-control-2'})
+        self.fields['description'].widget.attrs.update({'rows': '2'})
+
 
 class NftForm(forms.ModelForm):
     class Meta:
