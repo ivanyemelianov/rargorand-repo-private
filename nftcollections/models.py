adbdb1030fede2f42b8e033a1fb70a0b9850697b
Evan <ivan.emelyanov.94@gmail.com>
2022-02-09 09:48:42 -0800

    Updated home view and template

--- nftcollections/models.py
+++ nftcollections/models.py
@@ -49,7 +49,7 @@ class NftCollection(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     name = models.CharField(max_length=220)
     description = models.TextField(blank=True, null=True)
-    timestamp = models.DateTimeField(auto_now_add=True)
+    timestamp = models.DateField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     active = models.BooleanField(default=True)
     website = models.CharField(max_length=220)
--- rargorand/views.py
+++ rargorand/views.py
@@ -2,11 +2,24 @@ import random
 from django.shortcuts import redirect, render, get_object_or_404
 
 from drops.models import Drop
+from nftcollections.models import NftCollection
 
 
 def home_view(request, *args, **kwargs):
     drop_queryset = Drop.objects.all()
+    last_six_drops_q = Drop.objects.filter().order_by('-id')[:6]
+    featured_q = NftCollection.objects.filter(featured=True)
+    last_eight_q = NftCollection.objects.filter().order_by('-id')[:11]
+    featured_drop_q = Drop.objects.filter(featured=True)
+    ma_drop_q = Drop.objects.filter(mostanticipated=True)
+    ff_drop_q = Drop.objects.filter(futurefavourite=True)
     context = {
         "object_list": drop_queryset,
+        "upcomming_drops": last_six_drops_q,
+        "featured_collection": featured_q,
+        "new_collections": last_eight_q,
+        "featured_drop": featured_drop_q,
+        "most_anticipated": ma_drop_q,
+        "future_favourite": ff_drop_q
     }
     return render(request, "home-view.html", context=context)
